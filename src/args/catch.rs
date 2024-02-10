f8fc0c1f8228ab9c315290f50f0e0cad905d15bb
Ashutosh <ashu.bhatta2005@gmail.com>
2024-02-10 23:03:39 +0530

    Minor changes

---
+++ src/args/catch.rs
@@ -0,0 +1,186 @@
+use crate::args::packet_capture::PacketCapture;
+use clap::{App, Arg, ArgMatches, SubCommand};
+use pcap::{Capture, Inactive, Precision, TimestampType};
+use std::cell::RefCell;
+
+fn is_tstamp_type(val: String) -> Result<(), String> {
+    let domain_set = vec![
+        "Host",
+        "HostLowPrec",
+        "HostHighPrec",
+        "Adapter",
+        "AdapterUnsynced",
+    ];
+    if domain_set.contains(&&val[..]) {
+        Ok(())
+    } else {
+        Err(format!("The value must be one of {:?}", domain_set))
+    }
+}
+
+fn is_i32(val: String) -> Result<(), String> {
+    match val.parse::<i32>() {
+        Ok(_) => Ok(()),
+        Err(err) => Err("Invalid value".to_string()),
+    }
+}
+
+fn is_precision_type(val: String) -> Result<(), String> {
+    let domain_set = vec!["Micro", "Nano"];
+    if domain_set.contains(&&val[..]) {
+        Ok(())
+    } else {
+        Err(format!("The value must be one of the following: {:?}", domain_set))
+    }
+}
+
+pub struct CaptureSubcommand {}
+
+impl<'a, 'b> CaptureSubcommand {
+    pub fn new() -> CaptureSubcommand {
+        CaptureSubcommand {}
+    }
+
+    pub fn get_subcommand(&self) -> App<'a, 'b> {
+        let run_args = vec![
+            Arg::with_name("device_handle")
+                .help("Mention the device interface")
+                .takes_value(true)
+                .long("device_handle"),
+            Arg::with_name("timeout")
+                .help("Set the read timeout for the catch. By default, this is 0, which it will block indefinitely.")
+                .takes_value(true)
+                .short("t")
+                .long("timeout")
+                .validator(is_i32),
+            Arg::with_name("promisc")
+                .help("Set promiscuous mode on or off. By default, this is off.")
+                .short("p")
+                .long("promisc"),
+            Arg::with_name("rfmon")
+                .help("Set rfmon mode on or off. The default is maintained by pcap.")
+                .short("r")
+                .long("rfmon"),
+            Arg::with_name("buffer_size")
+                .help("Set the buffer size for incoming packet data. The default is 1000000. This should always be larger than the snaplen.")
+                .takes_value(true)
+                .short("b")
+                .long("buffer_size")
+                .validator(is_i32),
+            Arg::with_name("snaplen")
+                .help("Set the snaplen size (the maximum length of a packet captured into the buffer). \
+                    Useful if you only want certain headers, but not the entire packet.The default is 65535.")
+                .takes_value(true)
+                .short("s")
+                .long("snaplen")
+                .validator(is_i32),
+            Arg::with_name("precision")
+                .help("Set the time stamp precision returned in captures (Micro/Nano).")
+                .takes_value(true)
+                .long("precision")
+                .short("p")
+                .validator(is_precision_type),
+            Arg::with_name("tstamp_type")
+                .help("Set the time stamp type to be used by a capture device \
+                    (Host / HostLowPrec / HostHighPrec / Adapter / AdapterUnsynced).")
+                .takes_value(true)
+                .long("tstamp_type")
+                .validator(is_tstamp_type),
+            Arg::with_name("filter")
+                .help("Set filter to the capture using the given BPF program string.")
+                .takes_value(true)
+                .long("filter")
+                .short("f"),
+            Arg::with_name("savefile")
+                .help("Save the captured packets to file.")
+                .takes_value(true)
+                .long("savefile")
+                .short("sf")
+        ];
+
+        SubCommand::with_name("catch")
+            .about("Catch packets from the network.")
+            .subcommand(SubCommand::with_name("list").about("List all the available network interfaces."))
+            .subcommand(
+                SubCommand::with_name("run")
+                    .about("Start catching packets.")
+                    .args(&run_args),
+            )
+    }
+
+    pub fn run_args(
+        &self,
+        device: RefCell<Capture<Inactive>>,
+        args: &ArgMatches,
+    ) -> RefCell<Capture<Inactive>> {
+        let mut device = device.into_inner();
+        // the validators will ensure we are passing the proper type, hence using unwrap is not a problem.
+        if let Some(temp) = args.value_of("timeout") {
+            device = device.timeout(temp.parse().unwrap());
+        }
+        if let Some(temp) = args.value_of("promisc") {
+            device = device.promisc(temp.parse().unwrap());
+        }
+        if let Some(temp) = args.value_of("rfmon") {
+            device = device.rfmon(temp.parse().unwrap());
+        }
+        if let Some(temp) = args.value_of("buffer_size") {
+            device = device.buffer_size(temp.parse().unwrap());
+        }
+        if let Some(temp) = args.value_of("snaplen") {
+            device = device.snaplen(temp.parse().unwrap());
+        }
+        if let Some(temp) = args.value_of("precision") {
+            device = device.precision(self.get_precision_type(temp).unwrap());
+        }
+        if let Some(temp) = args.value_of("timestamp_type") {
+            device = device.tstamp_type(self.get_tstamp_type(temp).unwrap());
+        }
+        RefCell::new(device)
+    }
+
+    pub fn start(&self, device: RefCell<Capture<Inactive>>, args: &ArgMatches) {
+        let device = device.into_inner();
+        let mut packet_capture = PacketCapture::new();
+
+        match device.open() {
+            Ok(mut cap_handle) => {
+                // Set pacp capture filters
+                if let Some(temp) = args.value_of("filter") {
+                    cap_handle
+                        .filter(temp, false)
+                        .expect("Filters invalid, please check the documentation.");
+                }
+
+                // To select between saving to file and printing to console.
+                if let Some(temp) = args.value_of("savefile") {
+                    packet_capture.save_to_file(cap_handle, temp);
+                } else {
+                    packet_capture.print_to_console(cap_handle);
+                }
+            }
+            Err(err) => {
+                eprintln!("{:?}", err);
+            }
+        }
+    }
+
+    fn get_precision_type(&self, val: &str) -> Result<Precision, ()> {
+        match val {
+            "Micro" => Ok(Precision::Nano),
+            "Nano" => Ok(Precision::Nano),
+            _ => Err(()),
+        }
+    }
+
+    fn get_tstamp_type(&self, val: &str) -> Result<TimestampType, ()> {
+        match val {
+            "Host" => Ok(TimestampType::Host),
+            "HostLowPrec" => Ok(TimestampType::HostLowPrec),
+            "HostHighPrec" => Ok(TimestampType::HostHighPrec),
+            "Adapter" => Ok(TimestampType::Adapter),
+            "AdapterUnsynced" => Ok(TimestampType::AdapterUnsynced),
+            _ => Err(()),
+        }
+    }
+}
---
+++ src/args/catch_packets.rs
@@ -0,0 +1,203 @@
+use crate::args::packet_parse::{PacketHeader, PacketParse, ParsedPacket};
+use pcap::{Active, Capture, Device};
+// use serde::Serialize;
+// use std::fs;
+// use std::net::IpAddr;
+// use std::sync::{Arc, Mutex};
+// use threadpool::ThreadPool;
+
+pub struct PacketCapture {
+    err_count: u64,
+}
+
+impl PacketCapture {
+    pub fn new() -> PacketCapture {
+        PacketCapture { err_count: 0 }
+    }
+
+    pub fn list_devices() -> Result<(), pcap::Error> {
+        let devices: Vec<String> = Device::list()?.iter().map(|val| val.name.clone()).collect();
+        println!("All Interfaces : ");
+        devices.iter().for_each(|val| println!("* {}", val));
+        Ok(())
+    }
+
+    fn print_err(&mut self, err: String) {
+        self.err_count += 1;
+        eprintln!("ERROR {} : {}", self.err_count, err);
+    }
+
+    pub fn save_to_file(&mut self, mut cap_handle: Capture<Active>, file_name: &str) {
+        match cap_handle.savefile(&file_name) {
+            Ok(mut file) => {
+                while let Ok(packet) = cap_handle.next_packet() {
+                    file.write(&packet);
+                }
+            }
+            Err(err) => {
+                self.print_err(err.to_string());
+            }
+        }
+    }
+
+    pub fn print_to_console(&mut self, mut cap_handle: Capture<Active>) {
+        self.print_headers();
+
+        while let Ok(packet) = cap_handle.next_packet() {
+            let data = packet.data.to_owned();
+            let len = packet.header.len;
+            let ts: String = format!(
+                "{}.{:06}",
+                &packet.header.ts.tv_sec, &packet.header.ts.tv_usec
+            );
+
+            let packet_parse = PacketParse::new();
+            let parsed_packet = packet_parse.parse_packet(data, len, ts);
+            match parsed_packet {
+                Ok(parsed_packet) => {
+                    self.print_packet(&parsed_packet);
+                }
+                Err(err) => {
+                    self.print_err(err.to_string());
+                }
+            }
+        }
+    }
+
+    fn print_headers(&self) {
+        println!(
+            "{0: <25} | {1: <15} | {2: <25} | {3: <15} | {4: <15} | {5: <15} | {6: <35} |",
+            "Source IP", "Source Port", "Dest IP", "Dest Port", "Protocol", "Length", "Timestamp"
+        );
+        println!("{:-^1$}", "-", 165,);
+    }
+
+    fn get_packet_meta(&self, parsed_packet: &ParsedPacket) -> (String, String, String, String) {
+        let mut src_addr = "".to_string();
+        let mut dst_addr = "".to_string();
+        let mut src_port = "".to_string();
+        let mut dst_port = "".to_string();
+
+        parsed_packet.headers.iter().for_each(|pack| {
+            match pack {
+                PacketHeader::Tcp(packet) => {
+                    src_port = packet.source_port.to_string();
+                    dst_port = packet.dest_port.to_string();
+                }
+                PacketHeader::Udp(packet) => {
+                    src_port = packet.source_port.to_string();
+                    dst_port = packet.dest_port.to_string();
+                }
+                PacketHeader::Ipv4(packet) => {
+                    src_addr = packet.source_addr.to_string();
+                    dst_addr = packet.dest_addr.to_string();
+                }
+                PacketHeader::Ipv6(packet) => {
+                    src_addr = packet.source_addr.to_string();
+                    dst_addr = packet.dest_addr.to_string();
+                }
+                PacketHeader::Arp(packet) => {
+                    src_addr = packet.src_addr.to_string();
+                    dst_addr = packet.dest_addr.to_string();
+                }
+                _ => {}
+            };
+        });
+
+        (src_addr, src_port, dst_addr, dst_port)
+    }
+
+    fn print_packet(&self, parsed_packet: &ParsedPacket) {
+        let (src_addr, src_port, dst_addr, dst_port) = self.get_packet_meta(&parsed_packet);
+        let protocol = &parsed_packet.headers[0].to_string();
+        let length = &parsed_packet.len;
+        let ts = &parsed_packet.timestamp;
+        println!(
+            "{0: <25} | {1: <15} | {2: <25} | {3: <15} | {4: <15} | {5: <15} | {6: <35}",
+            src_addr, src_port, dst_addr, dst_port, protocol, length, ts
+        );
+    }
+
+    // pub fn parse_from_file(
+    //     &mut self,
+    //     file_name: &str,
+    //     save_file_path: Option<&str>,
+    //     filter: Option<String>,
+    // ) {
+    //     let pool = ThreadPool::new(num_cpus::get() * 2);
+    //     match Capture::from_file(file_name) {
+    //         Ok(mut cap_handle) => {
+    //             let packets = Arc::new(Mutex::new(Vec::new()));
+
+    //             if let Some(filter) = filter {
+    //                 cap_handle
+    //                     .filter(&filter)
+    //                     .expect("Filters invalid, please check the documentation.");
+    //             }
+
+    //             while let Ok(packet) = cap_handle.next() {
+    //                 let data = packet.data.to_owned();
+    //                 let len = packet.header.len;
+    //                 let ts: String = format!(
+    //                     "{}.{:06}",
+    //                     &packet.header.ts.tv_sec, &packet.header.ts.tv_usec
+    //                 );
+
+    //                 let packets = packets.clone();
+
+    //                 pool.execute(move || {
+    //                     let packet_parse = PacketParse::new();
+    //                     let parsed_packet = packet_parse.parse_packet(data, len, ts);
+
+    //                     packets.lock().unwrap().push(parsed_packet);
+    //                 });
+    //             }
+
+    //             pool.join();
+
+    //             if let Some(path) = save_file_path {
+    //                 #[derive(Serialize)]
+    //                 struct ParsedPacket {
+    //                     // struct fields...
+    //                     pub len: u32,
+    //                     pub timestamp: String,
+    //                     pub headers: Vec<PacketHeader>,
+    //                     pub remaining: Vec<u8>,
+    //                 }
+
+    //                 let mut parsed_packets = vec![];
+
+    //                 let packets = packets.lock().unwrap();
+    //                 let packets = &*packets;
+    //                 packets.iter().for_each(|pkt| match pkt {
+    //                     Ok(pkt) => {
+    //                         parsed_packets.push(pkt);
+    //                     }
+    //                     Err(err) => {
+    //                         self.print_err(err.to_string());
+    //                     }
+    //                 });
+
+    //                 let packets = serde_json::to_string(&packets).unwrap();
+    //                 fs::write(path, packets).unwrap();
+    //             } else {
+    //                 let packets = packets.lock().unwrap();
+
+    //                 self.print_headers();
+
+    //                 packets.iter().for_each(|pack| match pack {
+    //                     Ok(pack) => {
+    //                         self.print_packet(pack);
+    //                     }
+    //                     Err(err) => {
+    //                         self.print_err(err.to_string());
+    //                     }
+    //                 })
+    //             }
+    //         }
+    //         Err(err) => {
+    //             self.print_err(err.to_string());
+    //         }
+    //     }
+    // }
+}
